c5e03166b6f5989fc4fb00f9ece2042f083a5624
Ken Mwangi <kenmwangi071@gmail.com>
2022-07-06 10:03:53 +0300

    Configured default Font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 
   plugins: [require('@tailwindcss/typography')],
 };
